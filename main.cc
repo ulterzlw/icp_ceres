733f3a63deeefc0c579e16aa320035e209618996
Your Name <you@example.com>
2021-05-12 10:24:15 +0800

    hessian approximated covariance

--- main.cc
+++ main.cc
@@ -40,42 +40,117 @@ struct AnalyticalErrorTerm : public ceres::SizedCostFunction<3, 6> {
   Eigen::Vector3d t;
 };
 
-int main(int argc, char **argv) {
-  std::vector<Eigen::Vector3d> source;
-  source.emplace_back(Eigen::Vector3d(0, 0, 0));
-  source.emplace_back(Eigen::Vector3d(1, 0, 0));
-  source.emplace_back(Eigen::Vector3d(0, 1, 0));
+struct AutoDiffErrorTerm {
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-  Sophus::SE3d
-      tf(Sophus::SO3d::exp(Eigen::Vector3d(0, 0, 1)), Eigen::Vector3d(1, 0, 0));
-  std::cout << "truth:\n" << tf.matrix() << std::endl;
-  std::vector<Eigen::Vector3d> target;
-  for (int i = 0; i < source.size(); ++i) {
-    target.emplace_back(tf * source.at(i));
+  AutoDiffErrorTerm(Eigen::Vector3d s, Eigen::Vector3d t) : s(s), t(t) {};
+
+  template<typename T>
+  bool operator()(const T* const parameters,
+                  T* residuals) const {
+    Eigen::Map<const Eigen::Matrix<T, 6, 1>> param(parameters);
+    Sophus::SE3<T> tf = Sophus::SE3<T>::exp(param);
+    Eigen::Map<Eigen::Matrix<T, 3, 1>> r(residuals);
+
+    r = t - tf * s;
+    return true;
+  }
+  static ceres::CostFunction* Create(const Eigen::Vector3d s,
+                                     Eigen::Vector3d t) {
+    return new ceres::AutoDiffCostFunction<AutoDiffErrorTerm, 3, 6>(
+        new AutoDiffErrorTerm(s, t));
   }
 
-  Eigen::Matrix<double, 6, 1> tf_calculated_parameter(Eigen::Matrix<double,
-                                                                    6,
-                                                                    1>::Random());
+ protected:
+  Eigen::Vector3d s;
+  Eigen::Vector3d t;
+};
+
+
+template<typename T>
+inline void buildDenseMatrix(const ceres::CRSMatrix Ain, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic> &A)
+{
+    A.resize(Ain.num_rows, Ain.num_cols);
+    A.setZero();
+    for (int r = 0; r < Ain.num_rows; ++r) {
+        for (int i = Ain.rows[r]; i < Ain.rows[r + 1]; ++i) {
+            A(r, Ain.cols[i]) = T(Ain.values[i]);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+  std::vector<Eigen::Vector3d> target;
+  target.emplace_back(Eigen::Vector3d(-1, -1, 1));
+  target.emplace_back(Eigen::Vector3d(1, -1, 1));
+  target.emplace_back(Eigen::Vector3d(-1, 1, 1));
+  target.emplace_back(Eigen::Vector3d(1, 1, 1));
+  target.emplace_back(Eigen::Vector3d(-1, -1, -1));
+  target.emplace_back(Eigen::Vector3d(1, -1, -1));
+  target.emplace_back(Eigen::Vector3d(-1, 1, -1));
+  target.emplace_back(Eigen::Vector3d(1, 1, -1));
+
+  for(auto i: target) std::cout<<"-t: "<<i.transpose()<<std::endl;
+  Sophus::SE3d
+      tf(Sophus::SO3d::exp(Eigen::Vector3d(0, 0, 0)), Eigen::Vector3d(0, 0, 0));
+  std::vector<Eigen::Vector3d> source;
+  for (int i = 0; i < target.size(); ++i) {
+    auto p = tf*target.at(i);
+    source.emplace_back(p);
+  }
+
+  //noise
+  // for(auto& i: source) i+= 0.1 * Eigen::Vector3d::Random();
+  for(auto& i: source) i+= Eigen::Vector3d(0.5,0,0);
+  for(auto i: source) std::cout<<"-s: "<<i.transpose()<<std::endl;
+
+  std::cout << "truth:\n" << tf.matrix().inverse() << std::endl;
+  // Eigen::Matrix<double, 6, 1> tf_calculated_parameter(Eigen::Matrix<double, 6, 1>::Random());
+  Eigen::Matrix<double, 6, 1> tf_calculated_parameter(Eigen::Matrix<double, 6, 1>::Zero());
   std::cout << "initial:\n"
             << Sophus::SE3d::exp(tf_calculated_parameter).matrix() << std::endl;
 
   ceres::Problem problem;
+  std::vector<double *> para_ids;
+  std::vector<ceres::internal::ResidualBlock *> res_ids;
+
 
   for (int i = 0; i < source.size(); ++i) {
-    ceres::CostFunction
-        *cost_function = new AnalyticalErrorTerm(source.at(i), target.at(i));
-    problem.AddResidualBlock(cost_function,
-                             NULL,
-                             tf_calculated_parameter.data());
+    ceres::CostFunction *cost_function = new AnalyticalErrorTerm(source.at(i), target.at(i));
+    // ceres::CostFunction *cost_function = AutoDiffErrorTerm::Create(source.at(i), target.at(i));
+    res_ids.push_back(problem.AddResidualBlock(cost_function, NULL, tf_calculated_parameter.data()));
   }
+  para_ids.push_back(tf_calculated_parameter.data());
   ceres::Solver::Options options;
   ceres::Solver::Summary summary;
-  ceres::Solve(options, &problem, &summary);
+  // ceres::Solve(options, &problem, &summary);
 
   std::cout << summary.BriefReport() << std::endl;
   std::cout << "result:\n"
-            << Sophus::SE3d::exp(tf_calculated_parameter).matrix();
+            << Sophus::SE3d::exp(tf_calculated_parameter).matrix()<<std::endl;
+
+  double cost;
+  ceres::CRSMatrix jaco_crs;
+  ceres::Problem::EvaluateOptions e_option;
+  e_option.parameter_blocks = para_ids;
+  e_option.residual_blocks = res_ids;
+  std::vector<double> residuals;
+
+  problem.Evaluate(e_option, &cost, &residuals, nullptr, &jaco_crs);
+  std::cout<<"cost: " << cost<<std::endl;
+  for(auto i: residuals) std::cout<<i<<" ";
+  std::cout<<std::endl;
+  Eigen::MatrixXd jaco;
+  buildDenseMatrix(jaco_crs, jaco);
+  Eigen::MatrixXd H = jaco.transpose() * jaco;
+  std::cout << "H: \n" << H << std::endl;
+  
+  std::cout<<jaco_crs.num_rows<<std::endl;
+  double s2 = cost / (jaco_crs.num_rows - 6);
+  Eigen::MatrixXd I = 1/(2*s2)*H;
+  std::cout<<"I: \n" << I << std::endl;
+  Eigen::MatrixXd cov = I.inverse();
+  std::cout<<"cov: \n" << cov << std::endl;
 
   return 0;
 
